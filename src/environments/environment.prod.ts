8bdc4ac4d2c71ccce4b9052f8b56e8b8230209d5
Noha M <33437197+NohaaAa@users.noreply.github.com>
2024-03-03 23:14:22 +0200

    Update launcher icons, splash images, and dark mode settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  weatherBaseURL: {
-    base: 'https://dataservice.accuweather.com',
-    locations: 'locations/v1',
-    forecast: 'forecasts/v1'
+  weatherAPIURL: {
+    locations: 'https://dataservice.accuweather.com/locations/v1',
+    forecasts: 'https://dataservice.accuweather.com/forecasts/v1',
+    imgURL: 'https://developer.accuweather.com/sites/default/files'
   },
-  // weatherAPIKey: 'yUtVZyJsrHQ4BC6gkJNKGPo4C4f29M4v',
-  weatherAPIKey: 'p7dqGbdrnhQVHQgAGEr7b1AzEC1rL5sM',
+  weatherAPIKey: 'yUtVZyJsrHQ4BC6gkJNKGPo4C4f29M4v',
+  // weatherAPIKey: 'p7dqGbdrnhQVHQgAGEr7b1AzEC1rL5sM',
 };
